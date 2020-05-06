97d167c57e89d3e698023517a91d7962e7cb58a9
Joel <joel.aparicio@globant.com>
2020-05-06 14:17:32 -0500

    [UPDATE] - Complement the Detail movie section

--- app/src/main/java/com/glob/movies/DetailsRegistry.kt
+++ app/src/main/java/com/glob/movies/DetailsRegistry.kt
@@ -1,4 +1,52 @@
 package com.glob.movies
 
+import com.glob.movies.contracts.DetailsContract
+import com.glob.movies.data.executors.JobExecutor
+import com.glob.movies.data.executors.UIThread
+import com.glob.movies.data.providers.MoviesDataProvider
+import com.glob.movies.data.remote.services.MovieServices
+import com.glob.movies.data.repositories.MoviesDataRepository
+import com.glob.movies.domain.executors.PostExecutorThread
+import com.glob.movies.domain.executors.ThreadExecutor
+import com.glob.movies.domain.providers.MoviesProvider
+import com.glob.movies.domain.repositories.MoviesRepository
+import com.glob.movies.presenters.DetailsPresenter
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class DetailsRegistry {
+
+    private val threadExecutor: ThreadExecutor by lazy {
+        JobExecutor()
+    }
+
+    private val postExecutorThread: PostExecutorThread by lazy {
+        UIThread()
+    }
+
+    private fun getMovieService(): MovieServices { //fixme evite do boilerplate
+        val gson = GsonBuilder()
+            .setLenient()
+            .create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        return retrofit.create(MovieServices::class.java)
+    }
+
+    private val moviesRepository: MoviesRepository by lazy {
+        MoviesDataRepository(getMovieService())
+    }
+
+    private val provider: MoviesProvider by lazy {
+        MoviesDataProvider(moviesRepository, threadExecutor, postExecutorThread)
+    }
+
+    fun provide(view: DetailsContract.View): DetailsContract.Presenter {
+        return DetailsPresenter(provider, view)
+    }
 }
--- app/src/main/java/com/glob/movies/adapters/MovieAdapter.kt
+++ app/src/main/java/com/glob/movies/adapters/MovieAdapter.kt
@@ -1,4 +1,62 @@
 package com.glob.movies.adapters
 
-class MovieAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.glob.movies.R
+import com.glob.movies.domain.dtos.MovieDto
+
+class MovieAdapter(
+    private var movieList: ArrayList<MovieDto> = arrayListOf(),
+    private val context: Context,
+    private val listener: OnItemSelectedListener
+) :
+    RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = movieList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val moview = movieList[position]
+        holder.title.text = moview.title
+        holder.item.setOnClickListener {
+            listener.onItemSelected(moview.id)
+        }
+        Glide.with(context)
+            .load(moview.frontImage)
+            .into(holder.poster)
+    }
+
+    fun refreshList(newMovies: List<MovieDto>) {
+        newMovies.forEach {
+            movieList.add(it)
+        }
+        notifyDataSetChanged()
+    }
+
+    interface OnItemSelectedListener {
+        fun onItemSelected(id: String)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val poster: ImageView
+        val title: TextView
+        val item: ConstraintLayout
+
+        init {
+            poster = itemView.findViewById(R.id.ivPoster)
+            title = itemView.findViewById(R.id.tvTitle)
+            item = itemView.findViewById(R.id.clItem)
+        }
+    }
 }
